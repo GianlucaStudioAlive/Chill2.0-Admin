903a5dfdd747080060722021197ada995c2ea7d3
GianlucaStudioAlive <gluca.felli@gmail.com>
2024-07-21 12:10:08 +0200

    added obfuscator

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
-// next.config.mjs
-
+import JavaScriptObfuscator from 'webpack-obfuscator';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Puoi aggiungere altre configurazioni se necessario
-  };
-  
-  export default nextConfig;
-  
+    webpack(config, { dev, isServer }) {
+        if (!dev) {
+          // Disable source maps in production
+          config.devtool = false;
+          
+          // Add JavaScript obfuscation
+          config.plugins.push(
+            new JavaScriptObfuscator({
+              rotateUnicodeArray: true,
+              // You can add more options to customize the obfuscation
+            }, ['excluded_bundle_name.js']) // Exclude specific bundles if needed
+          );
+        }
+    
+        return config;
+      },
+};
+
+export default nextConfig;
