aa1430c8b491e0c28c5a661612c867219936460b
GianlucaStudioAlive <gluca.felli@gmail.com>
2024-07-22 04:15:35 +0200

    qqqq
    qqq

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
-import JavaScriptObfuscator from 'webpack-obfuscator';
+// import JavaScriptObfuscator from 'webpack-obfuscator';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, { dev, isServer }) {
-        if (!dev) {
-          // Disable source maps in production
-          config.devtool = false;
+    // webpack(config, { dev, isServer }) {
+    //     if (!dev) {
+    //       // Disable source maps in production
+    //       config.devtool = false;
           
-          // Add JavaScript obfuscation
-          config.plugins.push(
-            new JavaScriptObfuscator({
-              rotateUnicodeArray: true,
-              // You can add more options to customize the obfuscation
-            }, ['excluded_bundle_name.js']) // Exclude specific bundles if needed
-          );
-        }
+    //       // Add JavaScript obfuscation
+    //       config.plugins.push(
+    //         new JavaScriptObfuscator({
+    //           rotateUnicodeArray: true,
+    //           // You can add more options to customize the obfuscation
+    //         }, ['excluded_bundle_name.js']) // Exclude specific bundles if needed
+    //       );
+    //     }
     
-        return config;
-      },
+    //     return config;
+    //   },
 };
 
 export default nextConfig;
