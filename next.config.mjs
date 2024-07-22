18e481a24fc732a5479f171a169137361767ad6d
GianlucaStudioAlive <gluca.felli@gmail.com>
2024-07-22 04:22:55 +0200

    qqq
    qq

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,22 @@
-import JavaScriptObfuscator from 'webpack-obfuscator';
-
+// import JavaScriptObfuscator from 'webpack-obfuscator';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config, { dev, isServer }) {
-    if (!dev) {
-      // Disable source maps in production
-      config.devtool = false;
-      
-      // Add JavaScript obfuscation
-      config.plugins.push(
-        new JavaScriptObfuscator({
-          rotateUnicodeArray: true,
-          exclude: ['api/**'], // Esclude la directory delle API
-        }, [])
-      );
-    }
-
-    return config;
-  },
+    // webpack(config, { dev, isServer }) {
+    //     if (!dev) {
+    //       // Disable source maps in production
+    //       config.devtool = false;
+          
+    //       // Add JavaScript obfuscation
+    //       config.plugins.push(
+    //         new JavaScriptObfuscator({
+    //           rotateUnicodeArray: true,
+    //           // You can add more options to customize the obfuscation
+    //         }, ['excluded_bundle_name.js']) // Exclude specific bundles if needed
+    //       );
+    //     }
+    
+    //     return config;
+    //   },
 };
 
 export default nextConfig;
